4e58f08eaf13dd112315f100f9937bc186b6b776
Rasim Sabanovic <rasimsa@maestralsolutions.com>
2020-09-15 20:00:37 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CreateItemComponent } from './create-item/create-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesListComponent,
+    ItemsListComponent,
+    CheckoutComponent,
+    CreateItemComponent
   ],
   imports: [
     BrowserModule
