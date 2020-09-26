0c11952c43a950510834e35d2045cbe4aa4df35d
mmesihovic <mmesihovic1@etf.unsa.ba>
2020-09-26 15:20:00 +0200

    servis

--- src/app/services/god.service.ts
+++ src/app/services/god.service.ts
@@ -7,23 +7,78 @@ import { Employee } from '../models/employee';
 import { Product } from '../models/product';
 import { Receipt } from '../models/receipt';
 
+export interface RequestBody {
+  name: string;
+  data: Array<any>[];
+}
 
 @Injectable()
 export class GodService {
 
-  constructor() {
+  private endpoints = {
+    saveResource : 'http://localhost:3000/resources/save',
+    getResource : 'http://localhost:3000/resources/get'
+  };
 
-  }
 
   public employees: Employee[];
   public products: Product[];
   public receipts: Receipt[];
+  private reqBody: RequestBody;
+
+  private getResourceBody(nameX: string, dataX: Array<any>[]) {
+    return {
+      name: nameX,
+      data: dataX
+    };
+  }
+
+  constructor(private http: HttpClient) {
+
+    this.http.post(this.endpoints.getResource, this.getResourceBody('employees.json', []))
+    .subscribe( (x: any) => {
+      this.employees = x.data;
+    });
+    this.http.post(this.endpoints.getResource, this.getResourceBody('products.json', []))
+    .subscribe( (x: any) => {
+      this.products = x.data;
+    });
+    this.http.post(this.endpoints.getResource, this.getResourceBody('receipts.json', []))
+    .subscribe( (x: any) => {
+      this.receipts = x.data;
+    });
+
+  }
+
+  private saveResource(name: string, data: Array<any>) {
+    this.http.post(this.endpoints.saveResource, this.getResourceBody(name, data))
+    .subscribe( (x: any) => {
+      console.log(x);
+    });
+  }
+
+  private loadResource(name: string, data: Array<any>) {
+    this.http.post(this.endpoints.getResource, this.getResourceBody(name, data))
+    .subscribe( (x: any) => {
+      if (name === 'employees.json') {
+        this.employees = x.data;
+      } else if (name === 'products.json') {
+        this.products = x.data;
+      } else if (name === 'receipts.json') {
+              this.receipts = x.data;
+      }
+    });
+
+  }
+
 
   public getAllEmployees(): Employee[] {
+    this.loadResource('employees.json', []);
     return this.employees;
   }
 
   public getEmployeeByID(id: number): Employee {
+    this.loadResource('employees.json', []);
     for (const empC of this.employees) {
       if (empC.id === empC.id) {
         return empC;
@@ -33,6 +88,7 @@ export class GodService {
 
   public addEmployee(emp: Employee) {
     this.employees.push(emp);
+    this.saveResource('employees.json', this.employees);
     // Save all emp
   }
 
@@ -42,19 +98,23 @@ export class GodService {
         empC = emp;
       }
     }
+    this.saveResource('employees.json', this.employees);
   }
 
   public deleteEmployee(emp: Employee) {
     const index = this.employees.indexOf(emp);
     this.employees.splice(index);
+    this.saveResource('employees.json', this.employees);
   }
 
 
   public getAllProducts(): Product[] {
+    this.loadResource('products.json', []);
     return this.products;
   }
 
   public getProductByID(id: number): Product {
+    this.loadResource('products.json', []);
     for (const prod of this.products) {
       if (prod.id === id) {
         return prod;
@@ -64,6 +124,7 @@ export class GodService {
 
   public addProduct(prod: Product) {
     this.products.push(prod);
+    this.saveResource('products.json', this.products);
   }
 
   public updateProduct(prod: Product) {
@@ -72,18 +133,22 @@ export class GodService {
         prodC = prod;
       }
     }
+    this.saveResource('products.json', this.products);
   }
 
   public deleteProduct(prod: Product) {
     const index = this.products.indexOf(prod);
     this.products.splice(index);
+    this.saveResource('products.json', this.products);
   }
 
   public getAllReceipts(): Receipt[] {
+    this.loadResource('receipts.json', []);
     return this.receipts;
   }
 
   public getReceiptByID(id: number): Receipt {
+    this.loadResource('receipts.json', []);
     for (const rec of this.receipts) {
       if (rec.id === id) {
         return rec;
@@ -93,6 +158,7 @@ export class GodService {
 
   public addReceipt(rec: Receipt) {
     this.receipts.push(rec);
+    this.saveResource('receipts.json', this.receipts);
   }
 
   public stornReceipt(rec: Receipt) {
@@ -114,6 +180,8 @@ export class GodService {
       }));
     }
     this.receipts.splice(index);
+    this.saveResource('products.json', this.products);
+    this.saveResource('receipts.json', this.receipts);
   }
 
 
